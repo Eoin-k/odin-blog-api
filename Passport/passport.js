1951843398c8cdf8614d350cf964e26cd7d082f9
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-09-14 22:45:26 +0100

    More file setup

---
+++ Passport/passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+const { PrismaClient } = require("@prisma/client");
+
+const Prisma = new PrismaClient();
+const jwtSecret = process.env.jwtSecret;
+
+const options = {
+	secretOrKey: jwtSecret,
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+};
+
+passport.use(
+	new Strategy(options, async (jwt_payload, done) => {
+		try {
+			const user = await Prisma.user.findUnique({
+				where: { id: jwt_payload.userId },
+			});
+			if (user) {
+				return done(null, user);
+			} else {
+				return done(null, false);
+			}
+		} catch (err) {
+			return done(err, false);
+		}
+	}),
+);
+
+module;
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const cors = require("cors");
+
 const app = express();
 
 app.listen(5000, () => console.log("App running & listening on port 5000"));
